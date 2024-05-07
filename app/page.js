d92dabc2979d56f46e48bbb0c1c1c141b8bc141c
tylerroliverr <tyler.oliverjames@gmail.com>
2024-05-07 21:10:54 +1000

    sponge

--- app/page.js
+++ app/page.js
@@ -1,94 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="cursor"></div>
+      <div className="header">
+        <div className="logo">
+          <h1>$SHEEEEEEEEEESH</h1>
+        </div>
+        <div className="links">
+          <Link href="https://t.co/ImcSVY7lrm">
+            <p>TELEGRAM for $SHEEEEEEEE</p>
+          </Link>
+          <Link href="https://twitter.com/SHEEEEEEESH_SOL">
+            <p>TWITTER/X for $SHEEEEEEEE</p>
+          </Link>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="hero">
+        <div className="item">
+          <img src="/images2.jpg"></img>
+          <p>$SHEEEEEEEEE</p>
+        </div>
+        <div className="item">
+          <img src="/images1.webp"></img>
+          <p>caveman be like... WHERE MY $$$$SHEEEEEEEEEEEE</p>
+        </div>
+        <div className="item">
+          <img src="/images.jpg"></img>
+          <p>wait, do u have money???? oh no, YES!!! is that $SHHEEEEEE?!?!?!?</p>
+        </div>
+        <div className="item">
+          <img src="/images3.jpg"></img>
+          <p>mmm pls gimmie that $SHEEEE</p>
+        </div>
+        <div className="item">
+          <img src="/images4.jpg"></img>
+          <p>how i feel when i buy $SHEEEEE</p>
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="end">
+      <p>omg, pls i really need to buy SHEEEE rn......</p>
+      <p>nah, but fr, we are legit. we just love spongebob and his crew. SHEEEEEE</p>
       </div>
     </main>
   );
