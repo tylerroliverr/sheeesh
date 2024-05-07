3e13754e889d6c1e6de90c0d1d2e796e44ec1a10
tylerroliverr <tyler.oliverjames@gmail.com>
2024-05-07 21:25:55 +1000

    anayltics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
   );
