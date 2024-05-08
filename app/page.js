017ac0a50be4cab5e74d369cf043410efc11e94e
tylerroliverr <tyler.oliverjames@gmail.com>
2024-05-09 09:43:37 +1000

    added lore

--- app/page.js
+++ app/page.js
@@ -18,6 +18,29 @@ export default function Home() {
           </Link>
         </div>
       </div>
+      <div className="lore">
+          <p><span className="pop">The Lore</span>
+            <br></br>
+            Originally there was a token released on pump fun for this meme, it absolutely mooned from 5k to 50k in like 10 minutes, we then got rugged...
+            <br></br>
+            So community said fuck it we run this BACK... HARD!
+            <br></br>
+            It ran so hard dev came back in pump fun to apologize!</p>
+            <br></br>
+          <p>So naturally, WE RAN IT UP AGAIN,
+            Then CTO DEV rugs...
+            So a few leftover members decide;
+            <br></br>
+            FUCK THE JEETS
+            FUCK THE RUGGERS
+            <br></br>
+            IN WITH SHEESH
+            IN WITH RUN UPS
+            <br></br>
+            We have nothing to do with old teams, except we stole their gem of a memecoin and are going to moon it.
+            <br></br>
+            GG</p>
+        </div>
       <div className="hero">
         <div className="item">
           <img src="/images2.jpg"></img>
@@ -41,8 +64,8 @@ export default function Home() {
         </div>
       </div>
       <div className="end">
-      <p>omg, pls i really need to buy SHEEEE rn......</p>
-      <p>nah, but fr, we are legit. we just love spongebob and his crew. SHEEEEEE</p>
+        <p>omg, pls i really need to buy SHEEEE rn......</p>
+        <p>nah, but fr, we are legit. we just love spongebob and his crew. SHEEEEEE</p>
       </div>
     </main>
   );
